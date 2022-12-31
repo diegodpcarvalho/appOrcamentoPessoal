54fc3e4eee4b7584927559dc1f896211cd9a99be
diegodpcarvalho <diegodpcarvalho@gmail.com>
2022-12-31 01:04:18 -0300

    Listando Despesas

--- app.js
+++ app.js
@@ -40,6 +40,24 @@ class Bd {
         localStorage.setItem(id, JSON.stringify(d))
         localStorage.setItem('id',id)
     }
+
+    recuperarTodosRegistros() {
+
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        for(let i = 1; i <= id; i++){
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            if(despesa === null){
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -87,3 +105,38 @@ function cadastrarDespesa() {
     bd.gravar(despesa)
 }
  
+
+function carregaListaDespesa(){
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    despesas.forEach(
+        function(d){
+
+        
+        let linha = listaDespesas.insertRow()
+        
+        linha.insertCell(0).innerHTML = `${d.dia} / ${d.mes} / ${d.ano}`
+        
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
+
+}
